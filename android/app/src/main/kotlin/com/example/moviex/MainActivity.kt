df3ea9731fbd91de7b34cf4b488945b26649dee5
Iphang190909 <72132369+Iphang190909@users.noreply.github.com>
2022-07-05 17:05:01 +0700

    ganti

--- android/app/src/main/kotlin/com/example/moviex/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.moviex
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/utils/exception.dart
+++
@@ -1,5 +0,0 @@
-class DatasourceException implements Exception {
-  final String message;
-
-  const DatasourceException({this.message = 'Something went wrong 🙏.'});
-}
